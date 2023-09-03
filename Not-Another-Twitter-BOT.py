80128dea47037c1c82121c04e9edc2ed4df40145
John Connor <128232619+JohnConnorNPC@users.noreply.github.com>
2023-09-03 13:45:13 +1000

    Update Not-Another-Twitter-BOT.py

--- Not-Another-Twitter-BOT.py
+++ Not-Another-Twitter-BOT.py
@@ -1,20 +1,68 @@
 import base64
 import hashlib
 import os
-import json
 import re
+import sys
 import requests
 import sqlite3
 from datetime import datetime, timedelta
 from flask import Flask, request, redirect, session, g, jsonify
 from requests_oauthlib import OAuth2Session
 
+# Constants
+DATABASE = os.environ.get('DATABASE_NAME', 'tokens.db')
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+redirect_uri = os.environ.get("REDIRECT_URI", "http://127.0.0.1:5000/oauth/callback")
+auth_url = "https://twitter.com/i/oauth2/authorize"
+token_url = "https://api.twitter.com/2/oauth2/token"
+scopes = ["tweet.read", "users.read", "tweet.write", "offline.access"]
+
+# SQL Queries
+CREATE_TABLE_QUERY = '''
+    CREATE TABLE IF NOT EXISTS oauth_tokens (
+        id INTEGER PRIMARY KEY,
+        access_token TEXT,
+        refresh_token TEXT,
+        token_type TEXT,
+        expiry_timestamp INTEGER
+    )
+'''
+SELECT_TOKEN_QUERY = "SELECT access_token, refresh_token, token_type, expiry_timestamp FROM oauth_tokens LIMIT 1"
+DELETE_TOKENS_QUERY = "DELETE FROM oauth_tokens"
+INSERT_TOKEN_QUERY = "INSERT INTO oauth_tokens (access_token, refresh_token, token_type, expiry_timestamp) VALUES (?, ?, ?, ?)"
+
+
+# Initialization
 app = Flask(__name__)
 app.secret_key = os.urandom(50)
 
-# SQLite setup
-DATABASE = 'tokens.db'
+# Extract the IP and port using regex
+match = re.search(r'http://([\d\.]+):(\d+)', redirect_uri)
+if match:
+    host = match.group(1)
+    port = int(match.group(2))
+else:
+    print("Invalid REDIRECT_URI format.")
+    sys.exit(1)
 
+# Validate IP
+ip_pattern = r'^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})){3}$'
+if not re.match(ip_pattern, host):
+    host = "0.0.0.0"
+
+if not (1025 <= port <= 65535):
+    print("Invalid port number in REDIRECT_URI. Port must be between 1025 and 65535.")
+    sys.exit(1)
+
+# Code verifier and challenge for OAuth2 PKCE
+code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
+code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
+code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
+code_challenge = code_challenge.replace("=", "")
+
+# SQLite setup
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -27,36 +75,13 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-# Create the oauth_tokens table if it doesn't exist
 with app.app_context():
     db = get_db()
     cursor = db.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS oauth_tokens (
-            id INTEGER PRIMARY KEY,
-            access_token TEXT,
-            refresh_token TEXT,
-            token_type TEXT,
-            expiry_timestamp INTEGER
-        )
-    ''')
+    cursor.execute(CREATE_TABLE_QUERY)
     db.commit()
 
 # OAuth setup
-client_id = os.environ.get("CLIENT_ID")
-client_secret = os.environ.get("CLIENT_SECRET")
-auth_url = "https://twitter.com/i/oauth2/authorize"
-token_url = "https://api.twitter.com/2/oauth2/token"
-redirect_uri = os.environ.get("REDIRECT_URI")
-scopes = ["tweet.read", "users.read", "tweet.write", "offline.access"]
-
-code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
-code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
-
-code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
-code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
-code_challenge = code_challenge.replace("=", "")
-
 def make_token():
     return OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
 
@@ -72,21 +97,21 @@ def post_tweet(message, token):
         },
     )
 
-# Decoding methods
-def decode_base64(encoded_message):
-    return base64.b64decode(encoded_message).decode()
+def delete_tweet(tweet_id, token):
+    headers = {
+        "Authorization": "Bearer {}".format(token["access_token"]),
+        "Content-Type": "application/json",
+    }
+    response = requests.request("DELETE", f"https://api.twitter.com/2/tweets/{tweet_id}", headers=headers)
+    return response
 
-def decode_hex(encoded_message):
-    return bytes.fromhex(encoded_message).decode()
-
-def send_tweet_with_decoded_message(decoded_message, db, cursor):
-    if not decoded_message:
-        return jsonify({"error": "Message not provided"}), 400
-
-    cursor.execute("SELECT access_token, refresh_token, token_type, expiry_timestamp FROM oauth_tokens LIMIT 1")
+def get_or_refresh_token():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(SELECT_TOKEN_QUERY)
     data = cursor.fetchone()
     if not data:
-        return jsonify({"error": "No token found. Please authorize the app first."}), 400
+        return None, {"error": "No token found. Please authorize the app first."}, 400
 
     token = {
         'access_token': data[0],
@@ -105,38 +130,15 @@ def send_tweet_with_decoded_message(decoded_message, db, cursor):
                 refresh_token=token['refresh_token'],
             )
             token_expiry_timestamp = (datetime.now() + timedelta(seconds=refreshed_token['expires_in'])).timestamp()
-            cursor.execute("DELETE FROM oauth_tokens")
-            cursor.execute("INSERT INTO oauth_tokens (access_token, refresh_token, token_type, expiry_timestamp) VALUES (?, ?, ?, ?)", 
+            cursor.execute(DELETE_TOKENS_QUERY)
+            cursor.execute(INSERT_TOKEN_QUERY, 
                     (refreshed_token['access_token'], refreshed_token['refresh_token'], refreshed_token['token_type'], token_expiry_timestamp))
             db.commit()
-            token = refreshed_token
+            return refreshed_token, None, None
         except Exception as e:
-            return jsonify({"error": "Failed to refresh token. Reauthorization might be needed.", "details": str(e)}), 500
-
-    response = post_tweet(decoded_message, token)
-    if response.status_code != 201:
-        try:
-            twitter = make_token()
-            refreshed_token = twitter.refresh_token(
-                client_id=client_id,
-                client_secret=client_secret,
-                token_url=token_url,
-                refresh_token=token['refresh_token'],
-            )
-            token_expiry_timestamp = (datetime.now() + timedelta(seconds=refreshed_token['expires_in'])).timestamp()
-            cursor.execute("DELETE FROM oauth_tokens")
-            cursor.execute("INSERT INTO oauth_tokens (access_token, refresh_token, token_type, expiry_timestamp) VALUES (?, ?, ?, ?)", 
-                    (refreshed_token['access_token'], refreshed_token['refresh_token'], refreshed_token['token_type'], token_expiry_timestamp))
-            db.commit()
-            response = post_tweet(decoded_message, refreshed_token)
-            if response.status_code == 201:
-                return jsonify({"success": True, "message": "Tweet sent successfully after refreshing token!"})
-            else:
-                return jsonify({"error": "Failed to send tweet even after refreshing token.", "details": response.json()}), 500
-        except Exception as e:
-            return jsonify({"error": "Failed to send tweet and token refresh also failed.", "details": str(e)}), 500
-    else:
-        return jsonify({"success": True, "message": "Tweet sent successfully!"})
+            return None, {"error": "Failed to refresh token. Reauthorization might be needed.", "details": str(e)}, 500
+    
+    return token, None, None
 
 @app.route("/")
 def demo():
@@ -160,8 +162,8 @@ def callback():
     token_expiry_timestamp = (datetime.now() + timedelta(seconds=token['expires_in'])).timestamp()
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM oauth_tokens")
-    cursor.execute("INSERT INTO oauth_tokens (access_token, refresh_token, token_type, expiry_timestamp) VALUES (?, ?, ?, ?)", 
+    cursor.execute(DELETE_TOKENS_QUERY)
+    cursor.execute(INSERT_TOKEN_QUERY, 
                    (token['access_token'], token['refresh_token'], token['token_type'], token_expiry_timestamp))
     db.commit()
     return "Token stored successfully!"
@@ -171,38 +173,27 @@ def send_tweet_endpoint_plain():
     print(request.data)  # Log received data
     message = request.data.decode('utf-8')
 
-    db = get_db()
-    cursor = db.cursor()
-    return send_tweet_with_decoded_message(message, db, cursor)
+    token, error, status_code = get_or_refresh_token()
+    if error:
+        return jsonify(error), status_code
 
-@app.route('/sendtweet_base64', methods=['POST'])
-def send_tweet_endpoint_base64():
-    print(request.data)  # Log received data
-    encoded_message = request.json.get('message')
-    
-    if not encoded_message:
-        return jsonify({"error": "Message not provided"}), 400
+    response = post_tweet(message, token)
+    if response.status_code == 201:
+        return jsonify({"success": True, "message": "Tweet sent successfully!" , "response" : response.json()})
+    else:
+        return jsonify({"error": "Failed to send tweet.", "details": response.json()}), 500
 
-    decoded_message = decode_base64(encoded_message)
+@app.route('/deletetweet/<tweet_id>', methods=['DELETE'])
+def delete_tweet_endpoint(tweet_id):
+    token, error, status_code = get_or_refresh_token()
+    if error:
+        return jsonify(error), status_code
 
-    db = get_db()
-    cursor = db.cursor()
-    return send_tweet_with_decoded_message(decoded_message, db, cursor)
-
-@app.route('/sendtweet_hex', methods=['POST'])
-def send_tweet_endpoint_hex():
-    print(request.data)  # Log received data
-    encoded_message = request.json.get('message')
-    
-    if not encoded_message:
-        return jsonify({"error": "Message not provided"}), 400
-
-    decoded_message = decode_hex(encoded_message)
-
-    db = get_db()
-    cursor = db.cursor()
-    return send_tweet_with_decoded_message(decoded_message, db, cursor)
+    response = delete_tweet(tweet_id, token)
+    if response.status_code == 200:
+        return jsonify({"success": True, "message": "Tweet deleted successfully!"})
+    else:
+        return jsonify({"error": "Failed to delete tweet.", "details": response.json()}), 500
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(host=host, port=port)
